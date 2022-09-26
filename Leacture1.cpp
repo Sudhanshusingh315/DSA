4ee148ef79c023a1658b57a0faae6432bead9ce2
Sudhanshu <ss826063@gmail.com>
2022-09-26 21:09:55 +0530

    Initial commit

---
+++ Leacture1.cpp
@@ -0,0 +1,16 @@
+// Leacture 1 on Learing c++
+
+// iostream is the header file that contains all the modules
+
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  
+  cout<<"I like pizza"<<endl; 
+
+  return 0;
+
+}
