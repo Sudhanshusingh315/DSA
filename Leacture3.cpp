8166f852b65e929c1daba8f057849fb3725e3a06
Sudhanshusingh315 <ss826063@gmail.com>
2022-10-02 01:09:28 +0530

    Third commit

--- Leacture3.cpp
+++ Leacture3.cpp
@@ -1,3 +1,37 @@
-// This commit is to check if i add another file or not
-//
-// modified commit
+// Leacture 3 :- If-Else , While loops
+#include <iostream>
+using namespace std;
+
+// -- this is just the explation
+
+int main()
+{ 
+ // cin is for the input in c++
+
+
+  int  a = 0;
+
+  cin>>a;
+
+  if(a>0)
+  {
+    cout<<"A is Positive"<<endl;
+  
+  }
+  else {
+    cout<<"A is negative"<<endl;
+  }
+  return 0;
+}
+
+
+
+
+//  Notes for cin
+  /*
+
+   cin does not consider 
+  " "  :- space
+  "\t" :- tab
+  "\n" :- enter new line
+*/ 
