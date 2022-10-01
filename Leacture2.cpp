60ed6c9446f43a0c4345ca4d2d077e1e9b4c734d
Sudhanshusingh315 <ss826063@gmail.com>
2022-10-01 22:34:27 +0530

    Initial commit

--- Leacture2.cpp
+++ Leacture2.cpp
@@ -5,8 +5,37 @@
 using namespace std;
 int main()
 {
+  // datatypes :- int char float
+  int a = 5;
+  char ch = 'r';
+  
+
+  cout <<"This is int "<<a<<" and this is char "<<ch<<endl;
   
   return 0;
 }
 
+/*
+ 
 
+
+ int  = 4 byte = 32 bits and 1 byte has 8 bits 
+  
+    |_____________________| => limit is 2^32-1 (0/1) and minimus being 1
+
+  char =  1 byte = 8 bits 
+    |_____________________| => limit is 2^8 -1 (0/1) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
