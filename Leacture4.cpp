6aa41f7e65ff467f4e885dd94e4a3c37430c864d
Sudhanshusingh315 <ss826063@gmail.com>
2022-10-03 23:31:08 +0530

    Fifth commit

--- Leacture4.cpp
+++ Leacture4.cpp
@@ -1,3 +1,31 @@
 // PatternPrinting Programs
 
+/*
+*
+**
+***
+****
+*****
 
+ */
+
+
+
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  int n= 5 ;
+  for (int i = 1 ; i<=n ;i++ ) // This loop controls the rows  
+  {
+    for(int j = 1; j<=i ; j++) // This loops prints the starts
+    {
+      cout<<"*";
+    } 
+    // because we have only put endl so that it can go to the next line 
+    cout<<endl;
+  } 
+  return 0;
+}
