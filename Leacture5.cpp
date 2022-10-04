c1c61ddc3783f7b528cb764073dc4e0ad116be96
Sudhanshusingh315 <ss826063@gmail.com>
2022-10-04 23:33:17 +0530

    sixth commit

---
+++ Leacture5.cpp
@@ -0,0 +1,26 @@
+// Bitwise Operator and for Loops
+
+#include <iostream>
+using namespace std;
+int main ()
+{
+  cout<<"using Bitwise Operator"<<endl;
+  int a = 4;
+  int b = 6;
+  
+  cout<<"a&b "<< (a&b) <<endl;
+  cout<<"a|b "<< (a|b) <<endl;
+  cout<<"~a"<< (~a) <<endl;
+  cout<<"a^b "<< (a^b) <<endl;
+  
+  cout << "____________" <<endl;
+
+  cout <<"Followings are left shift and right shift"<<endl;
+  cout<<"17>>1 " <<(17>>1)<<endl;
+  cout<<"17>>2 "<<(17<<2)<<endl;
+  cout<<"19<<1 "<<(19<<1)<<endl;
+  cout<<"21<<2 "<<(21<<2)<<endl;
+
+
+  return 0;
+}
