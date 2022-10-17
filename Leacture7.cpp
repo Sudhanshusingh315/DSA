7466e1b638d0ffc6df85bb18c7d2e14f5d787151
Sudhanshusingh315 <ss826063@gmail.com>
2022-10-18 01:31:55 +0530

    thirteenth

--- Leacture7.cpp
+++ Leacture7.cpp
@@ -1,1 +1,28 @@
 // Problems solving with leetcode 
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+ // Problem 1: - Reverse Integer (Leetcode) 
+
+  int answer=0;
+  int numberToBeReversed = 0;
+  
+  cout<<"Enter the number"<<endl;
+
+  cin>>numberToBeReversed;  
+  while (numberToBeReversed!= 0)
+  {
+    int digit = numberToBeReversed%10;
+    
+    answer = (answer*10) + digit;
+    
+    numberToBeReversed = numberToBeReversed/10;
+  }
+  cout<<"answer"<<answer<<endl;
+
+
+  return 0;
+}
