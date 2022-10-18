2183d693fa64dfc88bc225a635674649606d3d11
Sudhanshusingh315 <ss826063@gmail.com>
2022-10-18 20:58:25 +0530

    fifteenth commit

--- Leacture7.cpp
+++ Leacture7.cpp
@@ -2,12 +2,13 @@
 #include <climits>
 #include <cstdint>
 #include <iostream>
+#include <math.h>
 using namespace std;
 int main()
 {
 
- // Problem 1: - Reverse Integer (Leetcode) 
-
+// Problem 1: - Reverse Integer (Leetcode) 
+/*
   int answer=0;
   int numberToBeReversed = 0;
   
@@ -23,8 +24,40 @@ int main()
     numberToBeReversed = numberToBeReversed/10;
   }
   cout<<"answer"<<answer<<endl;
+*/
 
 
+
+// Problem 2 :- Power of two  
+   int answer = 0;  
+   int numberThatPowerOfTwo =0;
+   bool ForT=0;
+   cout<<"Enter the number to be checked"<<endl;
+   cin>>numberThatPowerOfTwo;
+   for(int i=0 ; i<=30 ;i++)
+   {  
+      
+       answer = pow(2,i);
+       if(answer == numberThatPowerOfTwo) 
+       {
+         ForT = 1;
+
+            
+       }
+
+       
+  
+   }
+   
+   if(ForT == 1)
+   {
+     cout<<"Power of two"<<endl;
+     
+   }
+   else {
+      cout<<"not the power of two "<<endl;
+   }
+    
 
   return 0;
-}
+}  
