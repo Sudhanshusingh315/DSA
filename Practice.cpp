fa3410e6602cc09eaab3eef73968dbff4e450e20
Sudhanshusingh315 <ss826063@gmail.com>
2022-12-08 23:03:55 +0530

    test 4

--- Practice.cpp
+++ Practice.cpp
@@ -1,1 +1,10 @@
 // This is for the practice 
+// Bitwise Operator and for loops
+#include <iostream>
+using namespace std;
+int main ()
+{
+  cout<<(5<<2)<<endl;
+  cout<<(14>>2)<<endl;
+  return 0;
+}
